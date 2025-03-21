7da315fe81641c5a7a05a0b5847cd8918dcc9eb9
Oluwakorede Cole <ko@indicium.ag>
2025-03-21 20:56:20 +0100

    feat(users): add user model and basic user route with OpenAPI integration

---
+++ src/routes/users.rs
@@ -0,0 +1,26 @@
+use crate::{
+    api_config::AppState,
+    utils::{app_error::AppError, models::User},
+};
+use axum::Json;
+use std::sync::Arc;
+use utoipa_axum::{router::OpenApiRouter, routes};
+
+pub const USERS_TAG: &str = "users";
+
+pub(crate) fn router() -> OpenApiRouter<Arc<AppState>> {
+    OpenApiRouter::new().routes(routes!(get_user))
+}
+
+#[utoipa::path(
+    get,
+    path = "/user",
+    tag = USERS_TAG,
+    responses(
+        (status = 200,  body = User)
+    )
+)]
+#[tracing::instrument(name = "users::get_user")]
+async fn get_user() -> anyhow::Result<Json<User>, AppError> {
+    Ok(Json(User::default()))
+}
---
+++ src/utils/models.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+use ulid::Ulid;
+use utoipa::ToSchema;
+
+#[derive(ToSchema, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: Ulid,
+    pub first_name: String,
+    pub last_name: String,
+}
